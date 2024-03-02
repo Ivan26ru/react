c7e3a2c6f55d8a0608edca372d68284bc5f442e8
Ivan <it-1ivan@ya.ru>
2024-03-02 20:21:51 +0500

    add server no work

---
+++ cfg/webpack.server.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+	target: "node", // исключительно для node.js
+	mode: NODE_ENV || "development",
+	entry: path.resolve(__dirname, '../src/server/server.js'),
+	output: {
+		path: path.resolve(__dirname, '../dist/server'),
+		filename: "server.js"
+	},
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+	},
+	externals: [nodeExternals()], // удалить лишние модули из серверного конфига
+	module: {
+		rules: [{
+			test: /\.[tj]sx?$/,
+			use: ['ts-loader']
+		}]
+	},
+	optimization: {
+		minimize: false
+	}
+};
