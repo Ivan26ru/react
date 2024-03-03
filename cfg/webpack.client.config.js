0530af46e1ab9171ba219a59308b2cb7f26c9631
Ivan <it-1ivan@ya.ru>
2024-03-03 12:32:38 +0500

    no work css style

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -21,10 +21,28 @@ module.exports = {
 		filename: "client.js"
 	},
 	module: {
-		rules: [{
-			test: /\.[tj]sx?$/,
-			use: ['ts-loader']
-		}]
+		rules: [
+			{
+				test: /\.[tj]sx?$/,
+				use: ['ts-loader']
+			},
+			{
+				test: /\.css&/,
+				use: [
+					"style-loader",
+					{
+						loader: 'css-loader',
+						options: {
+							modules: {
+								mode: 'local',
+								localIdentName: '[name]__[local]--[hash:base64:5]',
+							}
+						}
+					}
+				]
+			}
+
+		]
 	},
 	devtool: setupDevtool()
 };
